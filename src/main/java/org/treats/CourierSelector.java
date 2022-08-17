2783060150c2d86b5124b78c5a3bb2f9dd22b6c7
Haseeb-Khalil <haseeb3927@gmail.com>
2022-08-17 16:34:00 +0100

    Getting the cheapest courier sorted :)

--- src/main/java/org/treats/CourierSelector.java
+++ src/main/java/org/treats/CourierSelector.java
@@ -5,11 +5,13 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+import java.util.stream.Collectors;
 
 public class CourierSelector {
 
@@ -36,19 +38,17 @@ public class CourierSelector {
     static Courier bobby = new Courier("Bobby", 5.0, 1.75, "09:00", "13:00", true);
     static Courier martin = new Courier("Martin", 3.0, 1.50, "09:00", "17:00", false);
     static Courier geoff = new Courier("Geoff", 4.0, 2.00, "10:00", "16:00", true);
+    static Courier john = new Courier("John", 4.5, 1.25, "10:00", "16:00", true);
 
     // Couriers List
     static List<Courier> couriers = new ArrayList<>(
-            Arrays.asList(bobby, martin, geoff));
+            Arrays.asList(bobby, martin, geoff, john));
 
 
     // Courier selector for Best courier of the hour
     public static Courier getBestSuitableCourier(Order order) throws Exception {
 
-
         LOGGER.log(Level.INFO, "Getting best suitable courier for order: " + "\n" + "Order Time: " + order.getOrderTime().format(DateTimeFormatter.ofPattern("HH:mm")) + "\n" + "Distance Between Shop and Customer: " + order.getDistance() + "miles" + "\n" + "Refrigeration Requirement: " + order.isRefrigeratedBoxRequired());
-
-
         if (order.getDistance() <= 0) {
             LOGGER.log(Level.WARNING, "Distance cannot be less than or equal to 0");
             throw new Exception("Correct Distance is required");
@@ -57,47 +57,48 @@ public class CourierSelector {
             LOGGER.log(Level.WARNING, "Order time is not between business hours: 09:00 - 17:00");
             throw new Exception("Order time is outside of working hours");
         }
-//        return couriers.stream()
-//                .filter(courier -> {
-//                    courier.isAvailable(order);
-//                    LOGGER.log(Level.INFO, "Best suitable courier for this order is: " + courier.getName() +
-//                            "\n" + "Working Hours: " + courier.getStartTime() + " - " + courier.getEndTime() +
-//                            "\n" + "Has Refrigerated Box: " + courier.isHasRefrigeratedBox() +
-//                            "\n" + "Price: £" + courier.getPricePerMile() + " per mile");
+
+        // THIS METHOD WORKS BUT IS NOT THE BEST WAY TO DO IT.//
+//        for (Courier courier : couriers) {
+//            if (courier.isAvailable(order)) {
 //
-//                    return false;
-//                })
-//                .findFirst()
-//                .orElseThrow(() -> new Exception("No suitable courier found for this order: " + "\n" + "Order Time: " + order.getOrderTime() + "\n" + "Refrigerated Box Required: " + order.isRefrigeratedBoxRequired() + "\n" + "Distance Between Customer And Restaurant: " + order.getDistance() + " miles."));
+//
+//                LOGGER.log(Level.INFO, "Best suitable courier for this order is: " + courier.getName() +
+//                        "\n" + "Working Hours: " + courier.getStartTime() + " - " + courier.getEndTime() +
+//                        "\n" + "Has Refrigerated Box: " + courier.isHasRefrigeratedBox() +
+//                        "\n" + "Price: £" + courier.getPricePerMile() + " per mile" +
+//                        "\n" + "Total Delivery Price For This Order is: £" + courier.getPricePerMile() * order.getDistance());
+//
+//            }
+//            return courier;
+//
+//        }
+//        throw new Exception("No courier is available for this order");
+//        Making a list of available couriers
 
-        for (Courier courier : couriers) {
-            if (courier.isAvailable(order)) {
-                LOGGER.log(Level.INFO, "Best suitable courier for this order is: " + courier.getName() +
-                        "\n" + "Working Hours: " + courier.getStartTime() + " - " + courier.getEndTime() +
-                        "\n" + "Has Refrigerated Box: " + courier.isHasRefrigeratedBox() +
-                        "\n" + "Price: £" + courier.getPricePerMile() + " per mile" +
-                        "\n" + "Total Delivery Price For This Order is: £" + courier.getPricePerMile() * order.getDistance());
-                return courier;
-            }
+        List<Courier> availableCourier = couriers.stream().filter(courier -> courier.isAvailable(order)).collect(Collectors.toList());
+
+        if (!availableCourier.isEmpty()) {
+            Comparator<Courier> comparator = Comparator.comparing(courier -> courier.getPricePerMile());
+            Courier cheapestCourier = availableCourier.stream().min(comparator).get();
+
+            LOGGER.log(Level.INFO, "Best suitable courier for this order is: " + cheapestCourier.getName() +
+                    "\n" + "Working Hours: " + cheapestCourier.getStartTime() + " - " + cheapestCourier.getEndTime() +
+                    "\n" + "Has Refrigerated Box: " + cheapestCourier.isHasRefrigeratedBox() +
+                    "\n" + "Price: £" + cheapestCourier.getPricePerMile() + " per mile" +
+                    "\n" + "Total Delivery Price For This Order is: £" + cheapestCourier.getPricePerMile() * order.getDistance());
+            return cheapestCourier;
+
+        } else {
+            LOGGER.log(Level.WARNING, "No suitable courier found for this order: " + "\n" + "Order Time: " + order.getOrderTime().format(DateTimeFormatter.ofPattern("HH:mm")) + "\n" + "Refrigerated Box Required: " + order.isRefrigeratedBoxRequired() + "\n" + "Distance Between Customer And Restaurant: " + order.getDistance() + " miles.");
+            throw new Exception("No suitable courier found for this order: " + "\n" + "Order Time: " + order.getOrderTime().format(DateTimeFormatter.ofPattern("HH:mm")) + "\n" + "Refrigerated Box Required: " + order.isRefrigeratedBoxRequired() + "\n" + "Distance Between Customer And Restaurant: " + order.getDistance() + " miles.");
+
         }
-        LOGGER.log(Level.WARNING, "No suitable courier found for this order: " + "\n" + "Order Time: " + order.getOrderTime() + "\n" + "Refrigerated Box Required: " + order.isRefrigeratedBoxRequired() + "\n" + "Distance Between Customer And Restaurant: " + order.getDistance() + " miles.");
-        throw new Exception("No suitable courier found for this order: " + "\n" + "Order Time: " + order.getOrderTime() + "\n" + "Refrigerated Box Required: " + order.isRefrigeratedBoxRequired() + "\n" + "Distance Between Customer And Restaurant: " + order.getDistance() + " miles.");
-
 
     }
-
-    // Get courier details
-//    public static String getCourierDetails(Order order) throws Exception {
-//        Courier bestCourier = getBestSuitableCourier(order);
-//        return "Best Courier Found: " + "\n" + "Name: " + bestCourier.getName() + "\n" + "Start Time: " + bestCourier.getStartTime() + "\n" + "End Time: " + bestCourier.getEndTime() + "\n" + "Charge Per Mile: £" + bestCourier.getPricePerMile() + "\n" + "Can Deliver up to: " + bestCourier.getMaxDistance() + " miles" + "\n" + "Has Refrigerated Box? " + bestCourier.isHasRefrigeratedBox();
-//    }
-//
-//    // Get delivery price
-//    public static String getDeliveryPrice(Order order) throws Exception {
-//        Courier bestCourier = getBestSuitableCourier(order);
-//        return "Total Delivery Price For This Order is: £" + bestCourier.getPricePerMile() * order.getDistance();
-//    }
-
-
 }
+
+
+
+
 
--- src/main/java/org/treats/Main.java
+++ src/main/java/org/treats/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // Find best courier service
 //        Order order = new Order("11:00", true, 5.0);  // Prints Bobby
-        Order order = new Order("09:30", false, 3.0); // Prints Martin
+//        Order order = new Order("09:30", false, 3.0); // Prints Martin
 //        Order order = new Order("13:00", true, 4.0); // Prints Geoff
 
 //        Order order = new Order("11:00", true, 0); // Throws Exception for distance.
-//        Order order = new Order("1300 AM", true, 4.0); // Doesn't accept this format instead uses local time of device.
+        Order order = new Order("", true, 3.0); // Doesn't accept this format instead uses local time of device.
         CourierSelector.getBestSuitableCourier(order);
 //        System.out.println(CourierSelector.getCourierDetails(order));
 //        System.out.println(CourierSelector.getDeliveryPrice(order));
